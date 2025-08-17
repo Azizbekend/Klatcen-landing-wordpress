9eb0919db947d90f68ae024d7098927285d9ce05
azizbekktits <>
2025-08-17 21:39:27 +0300

    ergh

--- wp-content/themes/Project/#src/js/app.js
+++ wp-content/themes/Project/#src/js/app.js
@@ -8,17 +8,17 @@
 // @@-include('CheckThen/SlideToggle.js', {})
 
 // === components ===
-// @@-include('components/Popups/functions.js', {})
-// @@-include('components/Popups/Cookies.js', {})
+@@include('components/Popups/functions.js', {})
+@@include('components/Popups/Cookies.js', {})
 // @@-include('components/Gallery.js', {})
 // @@-include('components/SearchInList.js', {})
-// @@-include('components/Spollers.js', {})
-// @@-include('components/Swipers.js', {})
+@@include('components/Spollers.js', {})
+@@include('components/Swipers.js', {})
 
 // === functions ===
 // @@-include('functions/BodyLock.js', {})
 // @@-include('functions/Animate.js', {})
-// @@-include('functions/Menu.js', {})
+@@include('functions/Menu.js', {})
 // @@-include('functions/Tabs.js', {})
 
 // === ui-kit ===
@@ -26,7 +26,7 @@
 // @@-include('ui-kit/Quantity.js', {})
 // @@-include('ui-kit/Range.js', {})
 // @@-include('ui-kit/Rating.js', {})
-// @@-include('ui-kit/Select.js', {})
+@@include('ui-kit/Select.js', {})
 
 // === script ===
 @@include('script.js', {})
